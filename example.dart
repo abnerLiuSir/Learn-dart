97e880f7b6ee707a24f429872b18a07a10c1185e
abnerLiu <abnerliusir@163.com>
2020-02-09 23:22:25 +0800

    core

--- example.dart
+++ example.dart
@@ -29,11 +29,12 @@ void main() {
 		year += 1;
 	}
 	// 函数
-	int fibonacci(int n) {
+	fibonacci(int n) {
 		if (n == 0 || n == 1) return n;
 		return fibonacci(n - 1) + fibonacci(n - 2);
 	}
 
 	var result = fibonacci(20);
+	print(result);
 }
 

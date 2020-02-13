ac39c95bc97f268b9997cf48787d7a57c3e6256b
abnerLiu <abnerliusir@163.com>
2020-02-13 23:20:04 +0800

    2-13

--- 内建类型.dart
+++ 内建类型.dart
@@ -7,4 +7,29 @@ main() {
 	 */
 	var x = 1;
 	var hex = 0xDEADBEEF;
+	/**
+	 * String
+	 * Dart 字符串是一组 UTF-16 单元序列。 字符串通过单引号或者双引号创建。
+	 */
+	var s1 = 'Single quotes work well for string literals.';
+	var s2 = "Double quotes work just as well.";
+	/**
+	 * Boolean
+	 * Dart 使用 bool 类型表示布尔值。 Dart 只有字面量 true and false 是布尔类型， 这两个对象都是编译时常量
+	 */
+	// 检查空字符串。
+	var fullName = '';
+	print(fullName.isEmpty);
+
+	// 检查 0 值。
+	var hitPoints = 0;
+	print(hitPoints <= 0);
+
+	// 检查 null 值。
+	var unicorn;
+	print(unicorn == null);
+
+	// 检查 NaN 。
+	var iMeantToDoThis = 0 / 0;
+	print(iMeantToDoThis.isNaN);
 }

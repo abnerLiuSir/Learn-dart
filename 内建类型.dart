d692e73856429e88b1d1d32343538806756685c3
abnerLiu <abnerliusir@163.com>
2020-02-16 22:33:24 +0800

    2-16

--- 内建类型.dart
+++ 内建类型.dart
@@ -37,10 +37,29 @@ main() {
 	几乎每种编程语言中最常见的集合可能是 array 或有序的对象集合。
 	*/
 	var list = [1, 2, 3];
-	var list = [1, 2, 3];
-	assert(list.length == 3);
-	assert(list[1] == 2);
-
+	print(list.length == 3);
+	print(list[1] == 2);
 	list[1] = 1;
-	assert(list[1] == 1);
+	print(list[1] == 1);
+	//在 List 字面量之前添加 const 关键字，可以定义 List 类型的编译时常量：
+	var constantList = const [1, 2, 3];
+	// constantList[1] = 1; // 取消注释会引起错误。
+	/**
+	 * Set
+		在 Dart 中 Set 是一个元素唯一且无需的集合。
+	 */
+	var halogens = {'fluorine', 'chlorine', 'bromine', 'iodine', 'astatine'};
+	//要创建一个空集，使用前面带有类型参数的 {} ，或者将 {} 赋值给 Set 类型的变量：
+	var names = <String>{};
+	// Set<String> names = {}; // 这样也是可以的。
+	// var names = {}; // 这样会创建一个 Map ，而不是 Set 。
+	//因为先有的 Map 字母量语法，所以 {} 默认是 Map 类型。   如果忘记在 {} 上注释类型或赋值到一个未声明类型的变量上，
+	//那么 Dart 会创建一个类型为 Map<dynamic, dynamic> 的对象。
+
+	//使用 add() 或 addAll() 为已有的 Set 添加元素：
+
+	var elements = <String>{};
+	elements.add('fluorine');
+	elements.addAll(halogens);
+
 }

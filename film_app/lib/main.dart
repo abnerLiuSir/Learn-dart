47a6d62a11cfe42c29b29648e69707b68d10f33e
abnerLiu <abnerliusir@163.com>
2020-03-13 21:11:08 +0800

    package

--- film_app/lib/main.dart
+++ film_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(new MyApp());
 
@@ -66,6 +67,7 @@ class _MyHomePageState extends State<MyHomePage> {
               },
             ),
             RouterTestRoute(),
+            RandomWordsWidget(),
           ],
         ),
       ),
@@ -147,6 +149,18 @@ class RouterTestRoute extends StatelessWidget {
         },
         child: Text("打开提示页"),
       ),
+    );
+  }
+}
+
+class RandomWordsWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+   // 生成随机字符串
+    final wordPair = new WordPair.random();
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: new Text(wordPair.toString()),
     );
   }
 }

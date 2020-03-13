6087626bc93683963fc94d2bf71db677afcdffce
abnerLiu <abnerliusir@163.com>
2020-03-13 20:20:27 +0800

    route

--- film_app/lib/main.dart
+++ film_app/lib/main.dart
@@ -7,10 +7,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'Flutter Demo',
+      initialRoute:"/", //名为"/"的路由作为应用的home(首页)
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      routes:{
+        "new_page":(context) => NewRoute(),
+         "/":(context) => MyHomePage(title: 'Flutter Demo Home Page'), //注册首页路由
+      } ,
     );
   }
 }
@@ -54,12 +58,14 @@ class _MyHomePageState extends State<MyHomePage> {
               textColor: Colors.blue,
               onPressed: () {
               //导航到新路由   
-                Navigator.push( context,
-                  MaterialPageRoute(builder: (context) {
-                    return NewRoute();
-                  }));
+                Navigator.pushNamed(context, "new_page");
+                // Navigator.push( context,
+                //   MaterialPageRoute(builder: (context) {
+                //     return NewRoute();
+                //   }));
               },
             ),
+            RouterTestRoute(),
           ],
         ),
       ),
@@ -81,6 +87,65 @@ class NewRoute extends StatelessWidget {
       ),
       body: Center(
         child: Text("This is new route"),
+      ),
+    );
+  }
+}
+
+//路由传值
+class TipRoute extends StatelessWidget {
+  TipRoute({
+    Key key,
+    @required this.text,  // 接收一个text参数
+  }) : super(key: key);
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("提示"),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(18),
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Text(text),
+              RaisedButton(
+                onPressed: () => Navigator.pop(context, "我是返回值"),
+                child: Text("返回"),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class RouterTestRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: RaisedButton(
+        onPressed: () async {
+          // 打开`TipRoute`，并等待返回结果
+          var result = await Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) {
+                return TipRoute(
+                  // 路由参数
+                  text: "我是提示xxxx",
+                );
+              },
+            ),
+          );
+          //输出`TipRoute`路由返回结果
+          print("路由返回值: $result");
+        },
+        child: Text("打开提示页"),
       ),
     );
   }

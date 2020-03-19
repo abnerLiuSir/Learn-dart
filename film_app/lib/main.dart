00b931f26f7cfd9b50af4f7e9c1c7696f187b101
abnerLiu <abnerliusir@163.com>
2020-03-19 15:30:33 +0800

    button

--- film_app/lib/main.dart
+++ film_app/lib/main.dart
@@ -90,60 +90,100 @@ class NewRoute extends StatelessWidget {
       body: Container(
         child: Column(
           children: <Widget>[ 
-            Text("Hello world"*3,
-              textAlign: TextAlign.left,
+            // Text("Hello world"*3,
+            //   textAlign: TextAlign.left,
+            // ),
+            // Text("Hello world! I'm Jack. "*8,
+            //   textAlign: TextAlign.center,
+            // ),
+            // Text("Hello world",
+            //   textScaleFactor: 2.5,
+            // ),
+            // Text("Hello world",
+            //   style: TextStyle(
+            //     color: Colors.blue,
+            //     fontSize: 18.0,
+            //     height: 1.2,  
+            //     fontFamily: "Courier",
+            //     background: new Paint()..color=Colors.yellow,
+            //     decoration:TextDecoration.underline,
+            //     decorationStyle: TextDecorationStyle.dashed
+            //   ),
+            // ),
+            // Text.rich(TextSpan(
+            //   children: [
+            //     TextSpan(
+            //       text: "Home: "
+            //     ),
+            //     TextSpan(
+            //       text: "https://flutterchina.club",
+            //       style: TextStyle(
+            //         color: Colors.blue
+            //       ),  
+            //     ),
+            //   ]
+            // )),
+            // DefaultTextStyle(
+            //   //1.设置文本默认样式  
+            //   style: TextStyle(
+            //     color:Colors.red,
+            //     fontSize: 20.0,
+            //   ),
+            //   textAlign: TextAlign.start,
+            //   child: Column(
+            //     crossAxisAlignment: CrossAxisAlignment.start,
+            //     children: <Widget>[
+            //       Text("hello world"),
+            //       Text("I am Jack"),
+            //       Text("I am Jack",
+            //         style: TextStyle(
+            //           inherit: false, //2.不继承默认样式
+            //           color: Colors.grey
+            //         ),
+            //       ),
+            //     ],
+            //   ),
+            // ),
+            RaisedButton(
+              child: Text("raise"),
+              onPressed: () {},
             ),
-            Text("Hello world! I'm Jack. "*8,
-              textAlign: TextAlign.center,
+            FlatButton(
+              child: Text("flat"),
+              onPressed: () {},
             ),
-            Text("Hello world",
-              textScaleFactor: 2.5,
+            OutlineButton(
+              child: Text("Outline"),
+              onPressed: () {},
             ),
-            Text("Hello world",
-              style: TextStyle(
-                color: Colors.blue,
-                fontSize: 18.0,
-                height: 1.2,  
-                fontFamily: "Courier",
-                background: new Paint()..color=Colors.yellow,
-                decoration:TextDecoration.underline,
-                decorationStyle: TextDecorationStyle.dashed
-              ),
+            IconButton(
+              icon: Icon(Icons.thumb_up),
+              onPressed: () {},
             ),
-            Text.rich(TextSpan(
-              children: [
-                TextSpan(
-                  text: "Home: "
-                ),
-                TextSpan(
-                  text: "https://flutterchina.club",
-                  style: TextStyle(
-                    color: Colors.blue
-                  ),  
-                ),
-              ]
-            )),
-            DefaultTextStyle(
-              //1.设置文本默认样式  
-              style: TextStyle(
-                color:Colors.red,
-                fontSize: 20.0,
-              ),
-              textAlign: TextAlign.start,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text("hello world"),
-                  Text("I am Jack"),
-                  Text("I am Jack",
-                    style: TextStyle(
-                      inherit: false, //2.不继承默认样式
-                      color: Colors.grey
-                    ),
-                  ),
-                ],
-              ),
+            RaisedButton.icon(
+              icon: Icon(Icons.send),
+              label: Text("发送"),
+              onPressed: () {},
             ),
+            OutlineButton.icon(
+              icon: Icon(Icons.add),
+              label: Text("添加"),
+              onPressed: () {},
+            ),
+            FlatButton.icon(
+              icon: Icon(Icons.info),
+              label: Text("详情"),
+              onPressed: () {},
+            ),
+            RaisedButtongit(
+              color: Colors.blue,
+              highlightColor: Colors.blue[700],
+              colorBrightness: Brightness.dark,
+              splashColor: Colors.grey,
+              child: Text("Submit"),
+              shape:RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+              onPressed: () {},
+            )
           ]
         )
       ),

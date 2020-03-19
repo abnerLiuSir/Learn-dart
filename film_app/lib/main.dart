7ddbf3393075092c2ce476b6245dd3b8cb183801
abnerLiu <abnerliusir@163.com>
2020-03-19 15:09:53 +0800

    text

--- film_app/lib/main.dart
+++ film_app/lib/main.dart
@@ -87,193 +87,65 @@ class NewRoute extends StatelessWidget {
       appBar: AppBar(
         title: Text("New route"),
       ),
-      body: Center(
-        child: ParentWidgetC(),
-      ),
-    );
-  }
-}
-
-// class TapboxA extends StatefulWidget {
-//   TapboxA({Key key}) : super(key: key);
-
-//   @override
-//   _TapboxAState createState() => new _TapboxAState();
-// }
-
-// class _TapboxAState extends State<TapboxA> {
-//   bool _active = false;
-
-//   void _handleTap() {
-//     setState(() {
-//       _active = !_active;
-//     });
-//   }
-
-//   Widget build(BuildContext context) {
-//     return new GestureDetector(
-//       onTap: _handleTap,
-//       child: new Container(
-//         child: new Center(
-//           child: new Text(
-//             _active ? 'Active' : 'Inactive',
-//             style: new TextStyle(fontSize: 32.0, color: Colors.white),
-//           ),
-//         ),
-//         width: 200.0,
-//         height: 200.0,
-//         decoration: new BoxDecoration(
-//           color: _active ? Colors.lightGreen[700] : Colors.grey[600],
-//         ),
-//       ),
-//     );
-//   }
-// }
-// class ParentWidget extends StatefulWidget {
-//   @override
-//   _ParentWidgetState createState() => new _ParentWidgetState();
-// }
-
-// class _ParentWidgetState extends State<ParentWidget> {
-//   bool _active = false;
-
-//   void _handleTapboxChanged(bool newValue) {
-//     setState(() {
-//       _active = newValue;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return new Container(
-//       child: new TapboxB(
-//         active: _active,
-//         onChanged: _handleTapboxChanged,
-//       ),
-//     );
-//   }
-// }
-
-// //------------------------- TapboxB ----------------------------------
-
-// class TapboxB extends StatelessWidget {
-//   TapboxB({Key key, this.active: false, @required this.onChanged})
-//       : super(key: key);
-
-//   final bool active;
-//   final ValueChanged<bool> onChanged;
-
-//   void _handleTap() {
-//     onChanged(!active);
-//   }
-
-//   Widget build(BuildContext context) {
-//     return new GestureDetector(
-//       onTap: _handleTap,
-//       child: new Container(
-//         child: new Center(
-//           child: new Text(
-//             active ? 'Active' : 'Inactive',
-//             style: new TextStyle(fontSize: 32.0, color: Colors.white),
-//           ),
-//         ),
-//         width: 200.0,
-//         height: 200.0,
-//         decoration: new BoxDecoration(
-//           color: active ? Colors.lightGreen[700] : Colors.grey[600],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-class ParentWidgetC extends StatefulWidget {
-  @override
-  _ParentWidgetCState createState() => new _ParentWidgetCState();
-}
-
-class _ParentWidgetCState extends State<ParentWidgetC> {
-  bool _active = false;
-
-  void _handleTapboxChanged(bool newValue) {
-    setState(() {
-      _active = newValue;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Container(
-      child: new TapboxC(
-        active: _active,
-        onChanged: _handleTapboxChanged,
-      ),
-    );
-  }
-}
-
-//----------------------------- TapboxC ------------------------------
-
-class TapboxC extends StatefulWidget {
-  TapboxC({Key key, this.active: false, @required this.onChanged})
-      : super(key: key);
-
-  final bool active;
-  final ValueChanged<bool> onChanged;
-
-  @override
-  _TapboxCState createState() => new _TapboxCState();
-}
-
-class _TapboxCState extends State<TapboxC> {
-  bool _highlight = false;
-
-  void _handleTapDown(TapDownDetails details) {
-    setState(() {
-      _highlight = true;
-    });
-  }
-
-  void _handleTapUp(TapUpDetails details) {
-    setState(() {
-      _highlight = false;
-    });
-  }
-
-  void _handleTapCancel() {
-    setState(() {
-      _highlight = false;
-    });
-  }
-
-  void _handleTap() {
-    widget.onChanged(!widget.active);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // 在按下时添加绿色边框，当抬起时，取消高亮  
-    return new GestureDetector(
-      onTapDown: _handleTapDown, // 处理按下事件
-      onTapUp: _handleTapUp, // 处理抬起事件
-      onTap: _handleTap,
-      onTapCancel: _handleTapCancel,
-      child: new Container(
-        child: new Center(
-          child: new Text(widget.active ? 'Active' : 'Inactive',
-              style: new TextStyle(fontSize: 32.0, color: Colors.white)),
-        ),
-        width: 200.0,
-        height: 200.0,
-        decoration: new BoxDecoration(
-          color: widget.active ? Colors.lightGreen[700] : Colors.grey[600],
-          border: _highlight
-              ? new Border.all(
-                  color: Colors.teal[700],
-                  width: 10.0,
-                )
-              : null,
-        ),
+      body: Container(
+        child: Column(
+          children: <Widget>[ 
+            Text("Hello world"*3,
+              textAlign: TextAlign.left,
+            ),
+            Text("Hello world! I'm Jack. "*8,
+              textAlign: TextAlign.center,
+            ),
+            Text("Hello world",
+              textScaleFactor: 2.5,
+            ),
+            Text("Hello world",
+              style: TextStyle(
+                color: Colors.blue,
+                fontSize: 18.0,
+                height: 1.2,  
+                fontFamily: "Courier",
+                background: new Paint()..color=Colors.yellow,
+                decoration:TextDecoration.underline,
+                decorationStyle: TextDecorationStyle.dashed
+              ),
+            ),
+            Text.rich(TextSpan(
+              children: [
+                TextSpan(
+                  text: "Home: "
+                ),
+                TextSpan(
+                  text: "https://flutterchina.club",
+                  style: TextStyle(
+                    color: Colors.blue
+                  ),  
+                ),
+              ]
+            )),
+            DefaultTextStyle(
+              //1.设置文本默认样式  
+              style: TextStyle(
+                color:Colors.red,
+                fontSize: 20.0,
+              ),
+              textAlign: TextAlign.start,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text("hello world"),
+                  Text("I am Jack"),
+                  Text("I am Jack",
+                    style: TextStyle(
+                      inherit: false, //2.不继承默认样式
+                      color: Colors.grey
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ]
+        )
       ),
     );
   }

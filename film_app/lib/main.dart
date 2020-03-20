6cf6f28c5146576e93da6fa9cfd524c5014ac5b6
abnerLiu <abnerliusir@163.com>
2020-03-20 21:54:18 +0800

    layout

--- film_app/lib/main.dart
+++ film_app/lib/main.dart
@@ -12,9 +12,10 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       routes:{
-        "progress_page":(context) => ProgressIndicator(),
+        "progress_page": (context) => ProgressIndicator(),
         "switchAndCheckBox_page": (context) => FormTestRoute(),
-         "/":(context) => MyHomePage(title: 'Flutter Demo Home Page'), //注册首页路由
+        "/":(context) => MyHomePage(title: 'Flutter Demo Home Page'), //注册首页路由
+        "layout_page": (context) => LayoutPageRoute(),
       } ,
     );
   }
@@ -67,6 +68,13 @@ class _MyHomePageState extends State<MyHomePage> {
               } ,
               icon: Icon(Icons.add), 
               label: Text("进度条")
+            ),
+            RaisedButton.icon(
+              onPressed: (){
+                Navigator.pushNamed(context, "layout_page");
+              }, 
+              icon: Icon(Icons.add), 
+              label: Text("布局")
             )
             // RouterTestRoute(),
             // RandomWordsWidget(),
@@ -272,5 +280,66 @@ class _ProgressRouteState extends State<ProgressRoute>
         ],
       ),
     );
+  }
+}
+
+class LayoutPageRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("布局"),
+      ),
+      body: Flow(
+        delegate: TestFlowDelegate(margin: EdgeInsets.all(10.0)),
+        children: <Widget>[
+          new Container(width: 80.0, height:80.0, color: Colors.red,),
+          new Container(width: 80.0, height:80.0, color: Colors.green,),
+          new Container(width: 80.0, height:80.0, color: Colors.blue,),
+          new Container(width: 80.0, height:80.0,  color: Colors.yellow,),
+          new Container(width: 80.0, height:80.0, color: Colors.brown,),
+          new Container(width: 80.0, height:80.0,  color: Colors.purple,),
+        ],
+      )
+    );
+  }
+}
+
+class TestFlowDelegate extends FlowDelegate {
+  EdgeInsets margin = EdgeInsets.zero;
+  TestFlowDelegate({this.margin});
+  @override
+  void paintChildren(FlowPaintingContext context) {
+    var x = margin.left;
+    var y = margin.top;
+    //计算每一个子widget的位置  
+    for (int i = 0; i < context.childCount; i++) {
+      var w = context.getChildSize(i).width + x + margin.right;
+      if (w < context.size.width) {
+        context.paintChild(i,
+            transform: new Matrix4.translationValues(
+                x, y, 0.0));
+        x = w + margin.left;
+      } else {
+        x = margin.left;
+        y += context.getChildSize(i).height + margin.top + margin.bottom;
+        //绘制子widget(有优化)  
+        context.paintChild(i,
+            transform: new Matrix4.translationValues(
+                x, y, 0.0));
+         x += context.getChildSize(i).width + margin.left + margin.right;
+      }
+    }
+  }
+
+  @override
+  getSize(BoxConstraints constraints){
+    //指定Flow的大小  
+    return Size(double.infinity,200.0);
+  }
+
+  @override
+  bool shouldRepaint(FlowDelegate oldDelegate) {
+    return oldDelegate != this;
   }
 }

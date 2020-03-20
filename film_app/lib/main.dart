ed9bf44fab4f69526815668dd4745849ab7ca2db
abnerLiu <abnerliusir@163.com>
2020-03-20 11:04:17 +0800

    progress

--- film_app/lib/main.dart
+++ film_app/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       routes:{
-        "new_page":(context) => NewRoute(),
+        "progress_page":(context) => ProgressIndicator(),
         "switchAndCheckBox_page": (context) => FormTestRoute(),
          "/":(context) => MyHomePage(title: 'Flutter Demo Home Page'), //注册首页路由
       } ,
@@ -54,24 +54,19 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.display1,
             ),
-            FlatButton(
-              child: Text("open new route"),
-              textColor: Colors.blue,
-              onPressed: () {
-              //导航到新路由   
-                Navigator.pushNamed(context, "new_page");
-                // Navigator.push( context,
-                //   MaterialPageRoute(builder: (context) {
-                //     return NewRoute();
-                //   }));
-              },
-            ),
             OutlineButton.icon(
               label: Text("选择框"),
               icon: Icon(Icons.add),
               onPressed: (){
                 Navigator.pushNamed(context, "switchAndCheckBox_page");
               },
+            ),
+            FlatButton.icon(
+              onPressed: (){
+                Navigator.pushNamed(context, "progress_page");
+              } ,
+              icon: Icon(Icons.add), 
+              label: Text("进度条")
             )
             // RouterTestRoute(),
             // RandomWordsWidget(),
@@ -87,221 +82,6 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class NewRoute extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("New route"),
-      ),
-      body: Scrollbar(
-        child: SingleChildScrollView(
-          child: Column( 
-            children: <Widget>[ 
-              // Text("Hello world"*3,
-              //   textAlign: TextAlign.left,
-              // ),
-              // Text("Hello world! I'm Jack. "*8,
-              //   textAlign: TextAlign.center,
-              // ),
-              // Text("Hello world",
-              //   textScaleFactor: 2.5,
-              // ),
-              // Text("Hello world",
-              //   style: TextStyle(
-              //     color: Colors.blue,
-              //     fontSize: 18.0,
-              //     height: 1.2,  
-              //     fontFamily: "Courier",
-              //     background: new Paint()..color=Colors.yellow,
-              //     decoration:TextDecoration.underline,
-              //     decorationStyle: TextDecorationStyle.dashed
-              //   ),
-              // ),
-              // Text.rich(TextSpan(
-              //   children: [
-              //     TextSpan(
-              //       text: "Home: "
-              //     ),
-              //     TextSpan(
-              //       text: "https://flutterchina.club",
-              //       style: TextStyle(
-              //         color: Colors.blue
-              //       ),  
-              //     ),
-              //   ]
-              // )),
-              // DefaultTextStyle(
-              //   //1.设置文本默认样式  
-              //   style: TextStyle(
-              //     color:Colors.red,
-              //     fontSize: 20.0,
-              //   ),
-              //   textAlign: TextAlign.start,
-              //   child: Column(
-              //     crossAxisAlignment: CrossAxisAlignment.start,
-              //     children: <Widget>[
-              //       Text("hello world"),
-              //       Text("I am Jack"),
-              //       Text("I am Jack",
-              //         style: TextStyle(
-              //           inherit: false, //2.不继承默认样式
-              //           color: Colors.grey
-              //         ),
-              //       ),
-              //     ],
-              //   ),
-              // ),
-              // RaisedButton(
-              //   child: Text("raise"),
-              //   onPressed: () {},
-              // ),
-              // FlatButton(
-              //   child: Text("flat"),
-              //   onPressed: () {},
-              // ),
-              // OutlineButton(
-              //   child: Text("Outline"),
-              //   onPressed: () {},
-              // ),
-              // IconButton(
-              //   icon: Icon(Icons.thumb_up),
-              //   onPressed: () {},
-              // ),
-              // RaisedButton.icon(
-              //   icon: Icon(Icons.send),
-              //   label: Text("发送"),
-              //   onPressed: () {},
-              // ),
-              // OutlineButton.icon(
-              //   icon: Icon(Icons.add),
-              //   label: Text("添加"),
-              //   onPressed: () {},
-              // ),
-              // FlatButton.icon(
-              //   icon: Icon(Icons.info),
-              //   label: Text("详情"),
-              //   onPressed: () {},
-              // ),
-              // RaisedButtongit(
-              //   color: Colors.blue,
-              //   highlightColor: Colors.blue[700],
-              //   colorBrightness: Brightness.dark,
-              //   splashColor: Colors.grey,
-              //   child: Text("Submit"),
-              //   shape:RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
-              //   onPressed: () {},
-              // )
-              // Row(
-              //   mainAxisAlignment: MainAxisAlignment.center,
-              //   children: <Widget>[
-              //     Icon(Icons.accessible,color: Colors.green,),
-              //     Icon(Icons.error,color: Colors.green,),
-              //     Icon(Icons.fingerprint,color: Colors.green,),
-              //   ],
-              // ),
-              // Image(
-              //   image: AssetImage("images/avatar.jpg"),
-              //   width: 100.0
-              // ),
-              // Image.asset("images/avatar.jpg",
-              //   width: 150.0
-              // ),
-              // Image(
-              //   image: NetworkImage("https://www.baidu.com/img/bd_logo1.png"),
-              //   width: 100.0,
-              // ),
-              // Image.network(
-              //   "https://www.baidu.com/img/bd_logo1.png",
-              //   width: 150.0,
-              // ),
-              // Image(
-              //   image: AssetImage("images/avatar.jpg"),
-              //   width: 100.0,
-              //   color: Colors.blue,
-              //   colorBlendMode: BlendMode.difference,
-              // ),
-              // Image(
-              //   image: AssetImage("images/avatar.jpg"),
-              //   width: 100.0,
-              //   height: 200.0,
-              //   repeat: ImageRepeat.repeatY ,
-              // )
-            ]
-          )
-        )
-      ),
-    );
-  }
-}
-
-class SwitchAndCheckBoxTestRoute extends StatefulWidget {
-  @override
-  _SwitchAndCheckBoxTestRouteState createState() => new _SwitchAndCheckBoxTestRouteState();
-}
-
-class _SwitchAndCheckBoxTestRouteState extends State<SwitchAndCheckBoxTestRoute> {
-  TextEditingController _unameController = TextEditingController();
-  TextEditingController _upasswordController = TextEditingController();
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title : Text("switch and checkbox"),
-      ),
-      body: Container (
-        child: Column(
-          children: <Widget>[
-            Theme(
-              data: Theme.of(context).copyWith(
-                  hintColor: Colors.grey[200], //定义下划线颜色
-                  inputDecorationTheme: InputDecorationTheme(
-                      labelStyle: TextStyle(color: Colors.red),//定义label字体样式
-                      hintStyle: TextStyle(color: Colors.yellow, fontSize: 14.0)//定义提示文本样式
-                  )
-              ),
-              child: Column(
-                children: <Widget>[
-                  TextField(
-                    decoration: InputDecoration(
-                        labelText: "用户名",
-                        hintText: "用户名或邮箱",
-                        prefixIcon: Icon(Icons.person)
-                    ),
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
-                        prefixIcon: Icon(Icons.lock),
-                        labelText: "密码",
-                        hintText: "您的登录密码",
-
-                    ),
-                    obscureText: true,
-                  )
-                ],
-              )
-            ),
-            Container(
-              child: TextField(
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                    labelText: "Email",
-                    hintText: "电子邮件地址",
-                    prefixIcon: Icon(Icons.email),
-                    border: InputBorder.none //隐藏下划线
-                )
-              ),
-              decoration: BoxDecoration(
-                  // 下滑线浅灰色，宽度1像素
-                  border: Border(bottom: BorderSide(color: Colors.grey[200], width: 1.0))
-              ),
-            )
-          ],
-        )
-      ),
-    );
-  }
-}
 class FormTestRoute extends StatefulWidget {
   @override
   _FormTestRouteState createState() => new _FormTestRouteState();
@@ -388,6 +168,108 @@ class _FormTestRouteState extends State<FormTestRoute> {
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class ProgressIndicator extends StatelessWidget {
+  @override 
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("进度条"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 24.0),
+        child: Column(
+          children: <Widget>[
+            LinearProgressIndicator(
+              backgroundColor: Colors.grey[200],
+              valueColor: AlwaysStoppedAnimation(Colors.blue),
+            ),
+            //进度条显示50%
+            LinearProgressIndicator(
+              backgroundColor: Colors.grey[200],
+              valueColor: AlwaysStoppedAnimation(Colors.blue),
+              value: .5, 
+            ),
+            CircularProgressIndicator(
+              backgroundColor: Colors.grey[200],
+              valueColor: AlwaysStoppedAnimation(Colors.blue),
+            ),
+            //进度条显示50%，会显示一个半圆
+            CircularProgressIndicator(
+              backgroundColor: Colors.grey[200],
+              valueColor: AlwaysStoppedAnimation(Colors.blue),
+              value: .5,
+              strokeWidth: 6.0 
+            ),
+            SizedBox(
+              height: 3,
+              width: 100,
+              child: LinearProgressIndicator(
+                backgroundColor: Colors.grey[200],
+                valueColor: AlwaysStoppedAnimation(Colors.blue),
+              ),
+            ),
+            // 圆形进度条直径指定为100
+            SizedBox(
+              height: 100,
+              width: 100,
+              child: CircularProgressIndicator(
+                backgroundColor: Colors.grey[200],
+                valueColor: AlwaysStoppedAnimation(Colors.blue),
+              ),
+            ),
+            ProgressRoute()
+          ],
+        ),
+      )
+    );
+  }
+}
+
+class ProgressRoute extends StatefulWidget {
+  @override
+  _ProgressRouteState createState() => _ProgressRouteState();
+}
+
+class _ProgressRouteState extends State<ProgressRoute>
+    with SingleTickerProviderStateMixin {
+  AnimationController _animationController;
+
+  @override
+  void initState() {
+    //动画执行时间3秒  
+    _animationController =
+        new AnimationController(vsync: this, duration: Duration(seconds: 3));
+    _animationController.forward();
+    _animationController.addListener(() => setState(() => {}));
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+            Padding(
+            padding: EdgeInsets.all(16),
+            child: LinearProgressIndicator(
+              backgroundColor: Colors.grey[200],
+              valueColor: ColorTween(begin: Colors.grey, end: Colors.blue)
+                .animate(_animationController), // 从灰色变成蓝色
+              value: _animationController.value,
+            ),
+          )
+        ],
       ),
     );
   }

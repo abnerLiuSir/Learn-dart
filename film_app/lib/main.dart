73a03515923ce4c688f37ebf786953759f39fc4b
abnerLiu <abnerliusir@163.com>
2020-03-25 23:02:12 +0800

    scroll view

--- film_app/lib/main.dart
+++ film_app/lib/main.dart
@@ -492,85 +492,53 @@ class ScrollRoutePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("滚动条")
       ),
-      body: ListView3()
+      body: ScrollNotificationTestRoute()
     );
   }
 }
 
-class ListView3 extends StatelessWidget {
+class ScrollNotificationTestRoute extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Column(children: <Widget>[
-      ListTile(title:Text("商品列表")),
-      Expanded(
-        child: ListView.builder(itemBuilder: (BuildContext context, int index) {
-          return ListTile(title: Text("$index"));
-        },itemCount: 10,),
-      )
-    ]);
-  }
-}
-class InfiniteListView extends StatefulWidget {
-  @override
-  _InfiniteListViewState createState() => new _InfiniteListViewState();
+  _ScrollNotificationTestRouteState createState() => _ScrollNotificationTestRouteState();
 }
 
-class _InfiniteListViewState extends State<InfiniteListView> {
-  static const loadingTag = "##loading##"; //表尾标记
-  var _words = <String>[loadingTag];
-
-  @override
-  void initState() {
-    super.initState();
-    _retrieveData();
-  }
+class _ScrollNotificationTestRouteState
+    extends State{
+  String _progress = "0%"; //保存进度百分比
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-      itemCount: _words.length,
-      itemBuilder: (context, index) {
-        //如果到了表尾
-        if (_words[index] == loadingTag) {
-          //不足100条，继续获取数据
-          if (_words.length - 1 < 100) {
-            //获取数据
-            _retrieveData();
-            //加载时显示loading
-            return Container(
-              padding: const EdgeInsets.all(16.0),
-              alignment: Alignment.center,
-              child: SizedBox(
-                  width: 24.0,
-                  height: 24.0,
-                  child: CircularProgressIndicator(strokeWidth: 2.0)
-              ),
-            );
-          } else {
-            //已经加载了100条数据，不再获取数据。
-            return Container(
-                alignment: Alignment.center,
-                padding: EdgeInsets.all(16.0),
-                child: Text("没有更多了", style: TextStyle(color: Colors.grey),)
-            );
-          }
-        }
-        //显示单词列表项
-        return ListTile(title: Text(_words[index]));
-      },
-      separatorBuilder: (context, index) => Divider(height: .0),
+    return Scrollbar( //进度条
+      // 监听滚动通知
+      child: NotificationListener<ScrollNotification>(
+        onNotification: (ScrollNotification notification) {
+          double progress = notification.metrics.pixels /
+              notification.metrics.maxScrollExtent;
+          //重新构建
+          setState(() {
+            _progress = "${(progress * 100).toInt()}%";
+          });
+          print("BottomEdge: ${notification.metrics.extentAfter == 0}");
+          //return true; //放开此行注释后，进度条将失效
+        },
+        child: Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            ListView.builder(
+                itemCount: 100,
+                itemExtent: 50.0,
+                itemBuilder: (context, index) {
+                  return ListTile(title: Text("$index"));
+                }
+            ),
+            CircleAvatar(  //显示进度百分比
+              radius: 30.0,
+              child: Text(_progress),
+              backgroundColor: Colors.black54,
+            )
+          ],
+        ),
+      ),
     );
-  }
-
-  void _retrieveData() {
-    Future.delayed(Duration(seconds: 2)).then((e) {
-      _words.insertAll(_words.length - 1,
-          //每次生成20个单词
-          generateWordPairs().take(20).map((e) => e.asPascalCase).toList()
-      );
-      setState(() {
-        //重新构建列表
-      });
-    });
   }
 }

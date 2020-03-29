b5ee124c0ca74fb965f91baceab3d888f49e557d
abnerLiu <abnerliusir@163.com>
2020-03-29 22:21:53 +0800

    features

--- film_app/lib/main.dart
+++ film_app/lib/main.dart
@@ -94,6 +94,8 @@ class _MyHomePageState extends State<MyHomePage> {
               icon: Icon(Icons.add_box), 
               label: Text("滚动")
             ),
+            WillPopScopeTestRoute(),
+            _TestWidget()
             // RouterTestRoute(),
             // RandomWordsWidget(),
           ],
@@ -248,7 +250,8 @@ class ProgressIndicator extends StatelessWidget {
                 valueColor: AlwaysStoppedAnimation(Colors.blue),
               ),
             ),
-            ProgressRoute()
+            ProgressRoute(),
+            
           ],
         ),
       )
@@ -535,6 +538,115 @@ class _ScrollNotificationTestRouteState
               radius: 30.0,
               child: Text(_progress),
               backgroundColor: Colors.black54,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+class WillPopScopeTestRoute extends StatefulWidget {
+  @override
+  WillPopScopeTestRouteState createState() {
+    return new WillPopScopeTestRouteState();
+  }
+}
+
+class WillPopScopeTestRouteState extends State<WillPopScopeTestRoute> {
+  DateTime _lastPressedAt; //上次点击时间
+
+  @override
+  Widget build(BuildContext context) {
+    return new WillPopScope(
+        onWillPop: () async {
+          if (_lastPressedAt == null ||
+              DateTime.now().difference(_lastPressedAt) > Duration(seconds: 1)) {
+            //两次点击间隔超过1秒则重新计时
+            _lastPressedAt = DateTime.now();
+            return false;
+          }
+          return true;
+        },
+        child: Container(
+          alignment: Alignment.center,
+          child: Text("1秒内连续按两次返回键退出"),
+        )
+    );
+  }
+}
+
+
+class ShareDataWidget extends InheritedWidget {
+  ShareDataWidget({
+    @required this.data,
+    Widget child
+  }) :super(child: child);
+
+  final int data; //需要在子树中共享的数据，保存点击次数
+
+  //定义一个便捷方法，方便子树中的widget获取共享数据  
+  static ShareDataWidget of(BuildContext context) {
+    return context.inheritFromWidgetOfExactType(ShareDataWidget);
+  }
+
+  //该回调决定当data发生变化时，是否通知子树中依赖data的Widget  
+  @override
+  bool updateShouldNotify(ShareDataWidget old) {
+    //如果返回true，则子树中依赖(build函数中有调用)本widget
+    //的子widget的`state.didChangeDependencies`会被调用
+    return old.data != data;
+  }
+}
+class _TestWidget extends StatefulWidget {
+  @override
+  __TestWidgetState createState() => new __TestWidgetState();
+}
+
+class __TestWidgetState extends State<_TestWidget> {
+  @override
+  Widget build(BuildContext context) {
+    //使用InheritedWidget中的共享数据
+    return Text(ShareDataWidget
+        .of(context)
+        .data
+        .toString());
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    //父或祖先widget中的InheritedWidget改变(updateShouldNotify返回true)时会被调用。
+    //如果build中没有依赖InheritedWidget，则此回调不会被调用。
+    print("Dependencies change");
+  }
+}
+
+class InheritedWidgetTestRoute extends StatefulWidget {
+  @override
+  _InheritedWidgetTestRouteState createState() => new _InheritedWidgetTestRouteState();
+}
+
+class _InheritedWidgetTestRouteState extends State<InheritedWidgetTestRoute> {
+  int count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return  Center(
+      child: ShareDataWidget( //使用ShareDataWidget
+        data: count,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20.0),
+              child: _TestWidget(),//子widget中依赖ShareDataWidget
+            ),
+            RaisedButton(
+              child: Text("Increment"),
+              //每点击一次，将count自增，然后重新build,ShareDataWidget的data将被更新  
+              onPressed: () => setState(() => ++count),
             )
           ],
         ),

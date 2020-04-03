ecee7c15d6f128258881f66f726da348a4360b87
abnerLiu <abnerliusir@163.com>
2020-04-03 20:20:35 +0800

    homePage

---
+++ film_app/lib/data.dart
@@ -0,0 +1,11 @@
+List<String> images = [
+  "images/image_03.jpg",
+  "images/image_02.jpg",
+  "images/image_01.jpg",
+];
+
+List<String> title = [
+  "The Dreaming Moon",
+  "擅长人像，6年经验",
+  "擅长人像，6年经验",
+];
--- film_app/lib/main.dart
+++ film_app/lib/main.dart
@@ -1,6 +1,9 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
-import 'dart:math' as math;
 import 'package:english_words/english_words.dart';
+import 'customIcons.dart';
+import 'data.dart';
+import 'home.dart';
 
 void main() => runApp(new MyApp());
 
@@ -17,121 +20,19 @@ class MyApp extends StatelessWidget {
       routes:{
         "progress_page": (context) => ProgressIndicator(),
         "switchAndCheckBox_page": (context) => FormTestRoute(),
-        "/":(context) => MyHomePage(title: 'Flutter Demo Home Page'), //注册首页路由
+        "/":(context) => HomeRoute(), //注册首页路由
         "layout_page": (context) => TabBarTest(),
         "clip_page": (context) => ClipTestRoute(),
         "scroll_page": (context) => ScrollRoutePage(),
         "theme_page": (context) => ThemeTestRoute(),
         "inherited_page": (context) => TestInheritedWidget(),
-        "provide_page": (context) => ProviderRoute()
+        "provide_page": (context) => ProviderRoute(),
+        "UI_page": (context) => UIRoute()
       } ,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(widget.title),
-      ),
-      body: new Center(
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
-            ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-            OutlineButton.icon(
-              label: Text("选择框"),
-              icon: Icon(Icons.add),
-              onPressed: (){
-                Navigator.pushNamed(context, "switchAndCheckBox_page");
-              },
-            ),
-            FlatButton.icon(
-              onPressed: (){
-                Navigator.pushNamed(context, "progress_page");
-              } ,
-              icon: Icon(Icons.add), 
-              label: Text("进度条")
-            ),
-            RaisedButton.icon(
-              onPressed: (){
-                Navigator.pushNamed(context, "layout_page");
-              }, 
-              icon: Icon(Icons.add), 
-              label: Text("布局")
-            ),
-            FlatButton.icon(
-              onPressed: (){
-                Navigator.pushNamed(context, "clip_page");
-              }, 
-              icon: Icon(Icons.add_a_photo), 
-              label: Text("裁剪")
-            ),
-            FlatButton.icon(
-              onPressed: (){
-                Navigator.pushNamed(context, "scroll_page");
-              }, 
-              icon: Icon(Icons.add_box), 
-              label: Text("滚动")
-            ),
-            // RouterTestRoute(),
-            // RandomWordsWidget(),
-            FlatButton.icon(
-              onPressed: (){
-                Navigator.pushNamed(context, "theme_page");
-              },
-              icon: Icon(Icons.home),
-              label: Text("主题")
-            ),
-            FlatButton.icon(
-              onPressed: (){
-                Navigator.pushNamed(context, "inherited_page");
-              }, 
-              icon: Icon(Icons.data_usage),
-              label: Text("数据共享")
-            ),
-            FlatButton.icon(
-              onPressed: () {
-                Navigator.pushNamed(context, "provide_page");
-              }, 
-              icon: Icon(Icons.flag), 
-              label: Text("状态管理")
-            )
-          ],
-        ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
 
 class FormTestRoute extends StatefulWidget {
   @override
@@ -879,6 +780,287 @@ class _ChangeNotifierProviderState<T extends ChangeNotifier> extends State<Chang
     return InheritedProvider<T>(
       data: widget.data,
       child: widget.child,
+    );
+  }
+}
+
+class UIRoute extends StatefulWidget {
+  @override
+  _UIRouteState createState() => new _UIRouteState();
+}
+
+var cardAspectRatio = 12.0 / 16.0;
+var widgetAspectRatio = cardAspectRatio * 1.2;
+
+class _UIRouteState extends State<UIRoute> {
+  var currentPage = images.length - 1.0;
+
+  @override
+  Widget build(BuildContext context) {
+    PageController controller = PageController(initialPage: images.length - 1);
+    controller.addListener(() {
+      setState(() {
+        currentPage = controller.page;
+      });
+    });
+
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color(0xFF77A69D),
+            Color(0xFF5FCDBA),
+            Color(0xFF0BF4D9),
+          ],
+              begin: Alignment.bottomCenter,
+              end: Alignment.topCenter,
+              tileMode: TileMode.clamp)),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 12.0, right: 12.0, top: 30.0, bottom: 8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    IconButton(
+                      icon: Icon(
+                        Icons.menu,
+                        color: Colors.white,
+                        size: 30.0,
+                      ),
+                      onPressed: () {},
+                    ),
+                    IconButton(
+                      icon: Icon(
+                        Icons.search,
+                        color: Colors.white,
+                        size: 30.0,
+                      ),
+                      onPressed: () {},
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20.0),
+                child: Center(
+                  child: Text("摄影师",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 46.0,
+                        fontFamily: "Calibre-Semibold",
+                        letterSpacing: 1.0,
+                      )),
+                ),
+              ),
+              // Padding(
+              //   padding: const EdgeInsets.only(left: 20.0),
+              //   child: Row(
+              //     children: <Widget>[
+              //       Container(
+              //         decoration: BoxDecoration(
+              //           color: Color(0xFFff6e6e),
+              //           borderRadius: BorderRadius.circular(20.0),
+              //         ),
+              //         child: Center(
+              //           child: Padding(
+              //             padding: EdgeInsets.symmetric(
+              //                 horizontal: 22.0, vertical: 6.0),
+              //             child: Text("Animated",
+              //                 style: TextStyle(color: Colors.white)),
+              //           ),
+              //         ),
+              //       ),
+              //       SizedBox(
+              //         width: 15.0,
+              //       ),
+              //       Text("25+ Stories",
+              //           style: TextStyle(color: Colors.blueAccent))
+              //     ],
+              //   ),
+              // ),
+              Stack(
+                children: <Widget>[
+                  CardScrollWidget(currentPage),
+                  Positioned.fill(
+                    child: PageView.builder(
+                      itemCount: images.length,
+                      controller: controller,
+                      reverse: true,
+                      itemBuilder: (context, index) {
+                        return Container();
+                      },
+                    ),
+                  )
+                ],
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20.0),
+                child: Center(
+                  child: Text("模特",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 46.0,
+                          fontFamily: "Calibre-Semibold",
+                          letterSpacing: 1.0,
+                        ))
+                ),
+              ),
+              // Padding(
+              //   padding: const EdgeInsets.only(left: 20.0),
+              //   child: Row(
+              //     children: <Widget>[
+              //       Container(
+              //         decoration: BoxDecoration(
+              //           color: Colors.blueAccent,
+              //           borderRadius: BorderRadius.circular(20.0),
+              //         ),
+              //         child: Center(
+              //           child: Padding(
+              //             padding: EdgeInsets.symmetric(
+              //                 horizontal: 22.0, vertical: 6.0),
+              //             child: Text("Latest",
+              //                 style: TextStyle(color: Colors.white)),
+              //           ),
+              //         ),
+              //       ),
+              //       SizedBox(
+              //         width: 15.0,
+              //       ),
+              //       Text("9+ Stories",
+              //           style: TextStyle(color: Colors.blueAccent))
+              //     ],
+              //   ),
+              // ),
+              // SizedBox(
+              //   height: 20.0,
+              // ),
+              // Row(
+              //   children: <Widget>[
+              //     Padding(
+              //       padding: EdgeInsets.only(left: 18.0),
+              //       child: ClipRRect(
+              //         borderRadius: BorderRadius.circular(20.0),
+              //         child: Image.asset("images/image_02.jpg",
+              //             width: 296.0, height: 222.0),
+              //       ),
+              //     )
+              //   ],
+              // )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CardScrollWidget extends StatelessWidget {
+  var currentPage;
+  var padding = 20.0;
+  var verticalInset = 20.0;
+
+  CardScrollWidget(this.currentPage);
+
+  @override
+  Widget build(BuildContext context) {
+    return new AspectRatio(
+      aspectRatio: widgetAspectRatio,
+      child: LayoutBuilder(builder: (context, contraints) {
+        var width = contraints.maxWidth;
+        var height = contraints.maxHeight;
+
+        var safeWidth = width - 2 * padding;
+        var safeHeight = height - 2 * padding;
+
+        var heightOfPrimaryCard = safeHeight;
+        var widthOfPrimaryCard = heightOfPrimaryCard * cardAspectRatio;
+
+        var primaryCardLeft = safeWidth - widthOfPrimaryCard;
+        var horizontalInset = primaryCardLeft / 2;
+
+        List<Widget> cardList = new List();
+
+        for (var i = 0; i < images.length; i++) {
+          var delta = i - currentPage;
+          bool isOnRight = delta > 0;
+
+          var start = padding +
+              max(
+                  primaryCardLeft -
+                      horizontalInset * -delta * (isOnRight ? 15 : 1),
+                  0.0);
+
+          var cardItem = Positioned.directional(
+            top: padding + verticalInset * max(-delta, 0.0),
+            bottom: padding + verticalInset * max(-delta, 0.0),
+            start: start,
+            textDirection: TextDirection.rtl,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(16.0),
+              child: Container(
+                decoration: BoxDecoration(color: Colors.white, boxShadow: [
+                  BoxShadow(
+                      color: Colors.black12,
+                      offset: Offset(3.0, 6.0),
+                      blurRadius: 10.0)
+                ]),
+                child: AspectRatio(
+                  aspectRatio: cardAspectRatio,
+                  child: Stack(
+                    fit: StackFit.expand,
+                    children: <Widget>[
+                      Image.asset(images[i], fit: BoxFit.cover),
+                      Align(
+                        alignment: Alignment.bottomLeft,
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Padding(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 16.0, vertical: 8.0),
+                              child: Text(title[i],
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 25.0,
+                                      fontFamily: "SF-Pro-Text-Regular")),
+                            ),
+                            SizedBox(
+                              height: 10.0,
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  left: 12.0, bottom: 12.0),
+                              child: Container(
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: 22.0, vertical: 6.0),
+                                decoration: BoxDecoration(
+                                    color: Colors.blueAccent,
+                                    borderRadius: BorderRadius.circular(20.0)),
+                                child: Text("Read Later",
+                                    style: TextStyle(color: Colors.white)),
+                              ),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
+          cardList.add(cardItem);
+        }
+        return Stack(
+          children: cardList,
+        );
+      }),
     );
   }
 }
